d275d27981bd2eed2655a2bb3d00cdc47d01c80f
gabrielderodrigues <gabrielderodrigues@hotmail.com>
2022-11-30 06:02:29 -0300

    Introdução a arrays.

--- CSharpCollections/Program.cs
+++ CSharpCollections/Program.cs
@@ -10,6 +10,44 @@ namespace CSharpCollections
     {
         static void Main(string[] args)
         {
+            string aulaIntro = "Introdução as Coleções";
+            string aulaModelando = "Modelando a Classe Aula";
+            string aulaSets = "Trabalhando com Conjuntos";
+
+            //string[] aulas = new string[]
+            //{
+            //    aulaIntro, 
+            //    aulaModelando, 
+            //    aulaSets
+            //};
+
+            string[] aulas = new string[3];
+            aulas[0] = aulaIntro;
+            aulas[1] = aulaModelando;
+            aulas[2] = aulaSets;
+
+            Imprimir(aulas);
+
+            Console.WriteLine(aulas[0]);
+            Console.WriteLine(aulas[aulas.Length - 1]);
+
+            aulas[0] = "Trabalhando com Array";
+            Imprimir(aulas);
+
+            Console.ReadLine();
+        }
+
+        private static void Imprimir(string[] aulas)
+        {
+            //foreach (string aula in aulas)
+            //{
+            //    Console.WriteLine(aula);
+            //}
+
+            for (int i = 0; i < aulas.Length; i++)
+            {
+                Console.WriteLine($"{i+1}° aula: {aulas[i]}");
+            }
         }
     }
 }
