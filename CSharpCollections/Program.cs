efd18d935b5f5a262b8463f8d15001f7ccd46512
gabrielderodrigues <gabrielderodrigues@hotmail.com>
2023-01-02 05:38:33 -0300

    Operações com Array

--- CSharpCollections/Program.cs
+++ CSharpCollections/Program.cs
@@ -34,16 +34,43 @@ namespace CSharpCollections
             aulas[0] = "Trabalhando com Array";
             Imprimir(aulas);
 
+            Console.WriteLine($"Aula modelando está no índice {Array.IndexOf(aulas, aulaModelando)}");
+            Console.WriteLine(Array.LastIndexOf(aulas, aulaModelando)); // pega a ultima ocorrencia do "aulaModelando" no array.
+
+            Array.Reverse(aulas);
+            Imprimir(aulas);
+
+            Array.Reverse(aulas);
+            Imprimir(aulas);
+
+            Array.Resize(ref aulas, 2);
+            Imprimir(aulas);
+
+            Array.Resize(ref aulas, 3);
+            Imprimir(aulas);
+
+            aulas[aulas.Length - 1] = "Conclusão";
+            Imprimir(aulas);
+
+            Array.Sort(aulas); // ordem alfabética
+            Imprimir(aulas);
+
+            string[] copia = new string[2];
+            Array.Copy(aulas, 1, copia, 0, 2);
+            Imprimir(copia);
+
+            string[] clone = (string[])aulas.Clone();
+            Imprimir(clone);
+
+            Array.Clear(clone, 1, 2);
+            Imprimir(clone);
+
             Console.ReadLine();
         }
 
         private static void Imprimir(string[] aulas)
         {
-            //foreach (string aula in aulas)
-            //{
-            //    Console.WriteLine(aula);
-            //}
-
+            Console.WriteLine("");
             for (int i = 0; i < aulas.Length; i++)
             {
                 Console.WriteLine($"{i+1}° aula: {aulas[i]}");
