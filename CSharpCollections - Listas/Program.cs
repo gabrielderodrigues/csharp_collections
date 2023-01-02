409211d73762dcadfd764396c25a863cac06046d
gabrielderodrigues <gabrielderodrigues@hotmail.com>
2023-01-02 06:13:57 -0300

    Introdução às Listas

---
+++ CSharpCollections - Listas/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpCollections___Listas
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string aulaIntro = "Introdução as Coleções";
+            string aulaModelando = "Modelando a Classe Aula";
+            string aulaSets = "Trabalhando com Conjuntos";
+
+            //List<string> aulas = new List<string>
+            //{
+            //    aulaIntro, 
+            //    aulaModelando, 
+            //    aulaSets
+            //};
+
+            List<string> aulas = new List<string>();
+            aulas.Add(aulaIntro);
+            aulas.Add(aulaModelando);
+            aulas.Add(aulaSets);
+
+            Imprimir(aulas);
+
+            Console.WriteLine("");
+            Console.WriteLine("Primeira Aula: " + aulas[0]);
+            Console.WriteLine("Primeira Aula: " + aulas.First());
+            Console.WriteLine("");
+            Console.WriteLine("Ultima Aula: " + aulas[aulas.Count() - 1]);
+            Console.WriteLine("Ultima Aula: " + aulas.Last());
+
+            Console.ReadLine();
+        }
+
+        private static void Imprimir(List<string> aulas)
+        {
+            Console.WriteLine("");
+            //for (int i = 0; i < aulas.Count(); i++)
+            //{
+            //    Console.WriteLine($"{i + 1}° aula: {aulas[i]}");
+            //}
+
+            aulas.ForEach(aula =>
+            {
+                Console.WriteLine($"Aula: {aula}");
+            });
+        }
+    }
+}
---
+++ CSharpCollections - Listas/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CSharpCollections - Listas")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Avanade")]
+[assembly: AssemblyProduct("CSharpCollections - Listas")]
+[assembly: AssemblyCopyright("Copyright © Avanade 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("990afbfe-31b7-41ae-b6d0-b9419da85809")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
