69dd7ae7d0ef1bb1692e4c317f3af323dcb93699
gabrielderodrigues <gabrielderodrigues@hotmail.com>
2023-01-09 06:13:11 -0300

    Lista somente leitura e encapsulando método add.

---
+++ CSharpCollections - Listas.Somente.Leitura/Entities/Curso.cs
@@ -0,0 +1,50 @@
+﻿using CSharpCollections___Lista.De.Objetos.Entities;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpCollections___Listas.Somente.Leitura.Entities
+{
+    public class Curso
+    {
+		private List<Aula> aulas;
+
+		private string instrutor;
+
+		private string nome;
+
+		public Curso(string instrutor, string nome)
+		{
+			this.instrutor = instrutor;
+			this.nome = nome;
+			this.aulas = new List<Aula>();
+		}
+
+		public IList<Aula> Aulas
+		{
+			get { return new ReadOnlyCollection<Aula>(aulas); }
+		}
+
+
+		public string Nome
+		{
+			get { return nome; }
+			set { nome = value; }
+		}
+
+
+		public string Instrutor
+		{
+			get { return instrutor; }
+			set { instrutor = value; }
+		}
+
+		internal void AddClass(Aula aula)
+		{
+			this.aulas.Add(aula);
+		}
+	}
+}
---
+++ CSharpCollections - Listas.Somente.Leitura/Program.cs
@@ -0,0 +1,33 @@
+﻿using CSharpCollections___Lista.De.Objetos.Entities;
+using CSharpCollections___Listas.Somente.Leitura.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpCollections___Listas.Somente.Leitura
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Curso csharpColecoes = new Curso("C# Collections", "Gabriel");
+            csharpColecoes.AddClass(new Aula("Trabalhando com Listas", 21));
+
+            Imprimir(csharpColecoes.Aulas);
+
+            Console.ReadLine();
+        }
+
+        private static void Imprimir(IList<Aula> aulas)
+        {
+            Console.Clear();
+
+            foreach (var aula in aulas)
+            {
+                Console.WriteLine(aula);
+            }
+        }
+    }
+}
