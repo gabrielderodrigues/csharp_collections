665c3f723ad1928360ceda17b02daa6c6cb0f56f
gabrielderodrigues <gabrielderodrigues@hotmail.com>
2023-01-09 05:59:14 -0300

    Trabalhando com Listas de objetos e sobreescrita de métodos.

---
+++ CSharpCollections - Lista.De.Objetos/Entities/Aula.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpCollections___Lista.De.Objetos.Entities
+{
+    public class Aula : IComparable<Aula>
+    {
+        private string titulo;
+        private int tempo;
+
+        public Aula(string titulo, int tempo)
+        {
+            this.titulo = titulo;
+            this.tempo = tempo;
+        }
+
+        public int CompareTo(Aula other)
+        {
+            return titulo.CompareTo(other.titulo);
+        }
+
+        public override string ToString()
+        {
+            return $"[Título: {titulo}, Tempo: {tempo} minutos]";
+        }
+    }
+}
---
+++ CSharpCollections - Lista.De.Objetos/Program.cs
@@ -0,0 +1,42 @@
+﻿using CSharpCollections___Lista.De.Objetos.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpCollections___Lista.De.Objetos
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var aulaIntro = new Aula("Introdução as Coleções", 20);
+            var aulaModelando = new Aula("Modelando a Classe Aula", 18);
+            var aulaSets = new Aula("Trabalhando com Conjuntos", 16);
+
+            List<Aula> aulas = new List<Aula>();
+            aulas.Add(aulaIntro);
+            aulas.Add(aulaModelando);
+            aulas.Add(aulaSets);
+
+            Imprimir(aulas);
+
+            aulas.Sort();
+            Imprimir(aulas);
+
+            Console.ReadLine();
+        }
+
+        private static void Imprimir(List<Aula> aulas)
+        {
+            Console.Clear();
+
+            foreach (var aula in aulas)
+            {
+                Console.WriteLine(aula);
+            }
+        }
+    }
+}
